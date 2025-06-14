0f3e818ce4ea07061632cc235e686ac9d9f9f224
Flavio <flaviomedinajr@outlook.com>
2025-06-14 06:14:19 -0400

    set up function for toggleTodo

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,19 @@ export default function App() {
       completed: false}
       ]
     })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos =>  {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed}
+        }
+        return todo
+      })
+    })
   }
 
   return (
@@ -41,8 +54,8 @@ export default function App() {
       return (
       <li key={todo.id}>
       <label>
-        <input type="checkbox" checked={todo.
-        completed} />
+        <input type="checkbox" checked={todo.completed} 
+        onChange={e => toggleTodo(todo.id, e.target.checked)} />
         {todo.title}
       </label>
       <button className="btn btn-danger">Delete</button>
