e600e4fa71233773e11d00e8ee0913893e1eb4bf
Flavio <flaviomedinajr@outlook.com>
2025-06-14 06:27:00 -0400

    updated delete function

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,12 @@ export default function App() {
         onChange={e => toggleTodo(todo.id, e.target.checked)} />
         {todo.title}
       </label>
-      <button className="btn btn-danger">Delete</button>
+      <button 
+        onClick={()=> deleteTodo(todo.id)} 
+        className="btn btn-danger"
+        >
+          Delete
+      </button>
     </li>
     )
     })}
